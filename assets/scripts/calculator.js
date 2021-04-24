e42057f5f31fa3f6cfc0d7fad1cdf2fcab9c55b9
Fadhil <fadhilahmad914@gmail.com>
2021-04-24 23:37:18 +0700

    added a single operator function, multiple operation is coming

--- assets/scripts/calculator.js
+++ assets/scripts/calculator.js
@@ -6,11 +6,57 @@ const calculator = {
 };
 
 function inputDigit(digit) {
-    const { displayValue } = calculator;
+    const { displayValue, waitingForSecondOperand } = calculator;
     // Overwrite `displayValue` if the current value is '0' otherwise append to it
-    calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
+    if (waitingForSecondOperand === true) {
+        calculator.displayValue = digit;
+        calculator.waitingForSecondOperand = false;
+    } else{
+        calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
+    }
+
+    console.log(calculator);
 }
-  
+
+function inputDecimal(dot) {
+    if (!calculator.displayValue.includes(dot)){
+        //append decimal point
+        calculator.displayValue += dot;
+    }
+}
+
+function handleOperator(nextOperator) {
+    const { firstOperand, displayValue, operator } = calculator;
+    // parsefloat converts the string content of displayValue to a floating-point number
+    const inputValue = parseFloat(displayValue);
+
+    // verify the firstOperand is null and the value is not NaN
+    if (firstOperand === null && !isNaN(inputValue)) {
+        calculator.firstOperand = inputValue;
+    } else if(operator){
+        const result = calculate(firstOperand, inputValue, operator);
+
+        calculator.displayValue = String(result);
+        calculator.firstOperand = result;
+    }
+    calculator.waitingForSecondOperand = true;
+    calculator.operator = nextOperator;
+    console.log(calculator);
+}
+
+function calculate (firstOperand, secondOperand, operator) {
+    if(operator === "+"){
+        return firstOperand + secondOperand;
+    } else if (operator === "-"){
+        return firstOperand - secondOperand;
+    } else if (operator === "*"){
+        return firstOperand * secondOperand;
+    } else if(operator === "/"){
+        return firstOperand / secondOperand;
+    }
+
+    return secondOperand;
+}
 
 function updateScreen() {
     // select the element with class of `calculator-screen`
@@ -34,12 +80,14 @@ keys.addEventListener('click', (event) => {
   }
 
   if (target.classList.contains('operator')) {
-    console.log('operator', target.value);
+    handleOperator(target.value);
+    updateScreen();
     return;
   }
 
   if (target.classList.contains('decimal')) {
-    console.log('decimal', target.value);
+    inputDecimal(target.value);
+    updateScreen();
     return;
   }
 
